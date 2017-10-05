366b4b3a2beeffd53a071be4c977f4a167e083d0
Philipp Edelmann <philipp.edelmann@newcastle.ac.uk>
2017-10-05 16:14:33 +0100

    rename mesa Matlab script and reverse arrays

--- scripts/mesa_profile.m
+++ scripts/mesa_profile.m
@@ -1,4 +1,4 @@
-function ret=mesa(fn)
+function ret=mesa_profile(fn)
 	fid = fopen(fn);
 
 	fgets(fid);
@@ -17,6 +17,10 @@ function ret=mesa(fn)
 	data.Properties.VariableNames(1:end-1) = labels;
 	ret = table2struct(data,'ToScalar',true);
 	ret = rmfield(ret, labels(end));
+        fields = fieldnames(ret)
+        for i=1:length(fields)
+                f = char(fields(i))
+                ret.(f) = ret.(f)(end:-1:1)
 	for i=2:length(slabels)-1
 		ret.(char(slabels(i))) = sscanf(char(svalues(i)),'%f');
 	end
